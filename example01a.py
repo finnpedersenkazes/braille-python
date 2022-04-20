0fb1b2e4fd282e8b54912ddb32ecec4c617a21c2
Finn Pedersen <finnpedersenfrance@gmail.com>
2022-04-20 02:58:34 +0200

    trying to understand the read key

--- example01a.py
+++ example01a.py
@@ -1,67 +1,71 @@
 import brlapi
 import errno
-import Xlib.keysymdef.miscellany
+# import Xlib.keysymdef.miscellany
 
 
 def init():
-  return {
-    'counter': 0,
-    'message': "Press home, winup/dn or tab to continue ..."
-  }
+    return {
+        'counter': 0,
+        'message': "Press home, winup/dn or tab to continue ..."
+    }
 
 def view(m):
-  print("Key %ld (%x %x %x %x) !" % (m['key'], m["type"], m["command"], m["argument"], m["flags"]))
+    if m['counter'] == 0:
+        k = b.expandKeyCode(brlapi.KEY_CMD_HOME)
+        print("Key (Type %x, Command %x, Argument %x, Flags %x) !" %(k["type"], k["command"], k["argument"], k["flags"]))
+        print('display size' + str(b.displaySize))
+        print('driver name'+str(b.driverName))
+        print(m['message'])
+        b.writeText(m['message'])
+        print()
+    else:
+        print("Key (Type %x, Command %x, Argument %x, Flags %x) !" %(m["type"], m["command"], m["argument"], m["flags"]))
+        print("Counter: %s" %(m['counter']))
+        print(m['message'][0:40])
+        print()
+        b.writeText("Count: %s, %s" %(m['counter'],m['message']))
 
-  if m['counter'] == 0:
-    print('display size' + str(b.displaySize))
-    print('driver name'+str(b.driverName))
-    print(m['message'])
-    b.writeText(m['message'])
-  else:
-    print("Counter: %s" %(m['counter']))
-    print(m['message'][0:40])
-    b.writeText("Count: %s, %s" %(m['counter'],m['message']))
+def update(m, keyCode):
+    k = b.expandKeyCode(keyCode)
+    m["type"] = k["type"]
+    m["command"] = k["command"]
+    m["argument"] = k["argument"]
+    m["flags"] = k["flags"]
 
-def update(m, key):
-  k = b.expandKeyCode(key)
-  m['key'] = key
-  m['type'] = k["type"]
-  m["command"] = k["command"]
-  m["argument"] = k["argument"]
-  m["flags"] = k["flags"]
-
-  if key == brlapi.KEY_CMD_HOME:
+#    if key == brlapi.KEY_CMD_HOME:
     m['counter'] = m['counter'] + 1
     m['message'] = "Home Button"
 
-  return m
+    return m
 
 
 try:
-  b = brlapi.Connection()
-  b.enterTtyMode(1)
-  b.ignoreKeys(brlapi.rangeType_all,[0])
+    b = brlapi.Connection()
+    b.enterTtyMode(1)
+    b.acceptKeys(brlapi.rangeType_all,[0])
+
+  # b.ignoreKeys(brlapi.rangeType_all,[0])
 
   # Accept the home, window up and window down braille commands
-  b.acceptKeys(brlapi.rangeType_command,[brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_HOME, brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_WINUP, brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_WINDN])
+  # b.acceptKeys(brlapi.rangeType_command,[brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_HOME, brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_WINUP, brlapi.KEY_TYPE_CMD|brlapi.KEY_CMD_WINDN])
 
   # Accept the tab key
-  b.acceptKeys(brlapi.rangeType_key,[brlapi.KEY_TYPE_SYM|Xlib.keysymdef.miscellany.XK_Tab])
+  # b.acceptKeys(brlapi.rangeType_key,[brlapi.KEY_TYPE_SYM|Xlib.keysymdef.miscellany.XK_Tab])
 
 
   # init model
-  model = init()
+    model = init()
 
   # loop
 
-  for i in range(1, 5):
-
+  #  for i in range(1, 5):
+    while model['counter'] < 20:
     # view model
-    view(model)
+        view(model)
 
-    key = b.readKey()
+        key = b.readKey()
 
-    model = update(model, key)
+        model = update(model, key)
 
   # b.writeText(None,1)
   # b.readKey()
@@ -77,22 +81,22 @@ try:
   #     text = "Press any key to exit                 ",
   #     orMask = "".center(21,underline) + "".center(19,chr(0)))
 
-  # b.acceptKeys(brlapi.rangeType_all,[0])
-  # b.readKey()
+    b.acceptKeys(brlapi.rangeType_all,[0])
+    b.readKey()
 
-  b.leaveTtyMode()
-  b.closeConnection()
+    b.leaveTtyMode()
+    b.closeConnection()
 
 except brlapi.ConnectionError as e:
-  if e.brlerrno == brlapi.ERROR_CONNREFUSED:
-    print("Connection to %s refused. BRLTTY is too busy..." %(e.host))
-  elif e.brlerrno == brlapi.ERROR_AUTHENTICATION:
-    print("Authentication with %s failed. Please check the permissions of %s" %(e.host,e.auth))
-  elif e.brlerrno == brlapi.ERROR_LIBCERR and (e.libcerrno == errno.ECONNREFUSED or e.libcerrno == errno.ENOENT):
-    print("Connection to %s failed. Is BRLTTY really running?" %(e.host))
-  else:
-    print("Connection to BRLTTY at %s failed: " %(e.host))
-  print(e)
-  print(e.brlerrno)
-  print(e.libcerrno)
+    if e.brlerrno == brlapi.ERROR_CONNREFUSED:
+        print("Connection to %s refused. BRLTTY is too busy..." %(e.host))
+    elif e.brlerrno == brlapi.ERROR_AUTHENTICATION:
+        print("Authentication with %s failed. Please check the permissions of %s" %(e.host,e.auth))
+    elif e.brlerrno == brlapi.ERROR_LIBCERR and (e.libcerrno == errno.ECONNREFUSED or e.libcerrno == errno.ENOENT):
+        print("Connection to %s failed. Is BRLTTY really running?" %(e.host))
+    else:
+        print("Connection to BRLTTY at %s failed: " %(e.host))
+    print(e)
+    print(e.brlerrno)
+    print(e.libcerrno)
 
