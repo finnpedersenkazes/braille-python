8091ea50d831042aab9d4ca0c61b09b8c624d8c2
Finn Pedersen <finnpedersenfrance@gmail.com>
2022-04-28 07:10:14 +0200

    colition detection

--- example03.py
+++ example03.py
@@ -57,96 +57,77 @@ def init():
         'points': 0 # 0..999
     }
 
-# Visualize the model on the braille displan
-def view(brl, m):
-    printLog(m)
-    if m['counter'] == 0:
-        brl.writeText(m['message'])
-    else:
-        brl.writeDots(helper(m))
-
 def helper(m):
     # fullCell = brlapi.DOT1 | brlapi.DOT2 | brlapi.DOT3 | brlapi.DOT4 | brlapi.DOT5 | brlapi.DOT6 | brlapi.DOT7 | brlapi.DOT8
     cells = []
 
-    # Display moving obstacles and player in position 1 
-    # for cells in range(5, 30)
-    cells.append(0)
+    # Calculated cursor dots    
+    cursorDots = 0
     if m['cursorPos'] == 0:
-        cells.append(brlapi.DOT7 | brlapi.DOT8)
+        cursorDots = brlapi.DOT7 | brlapi.DOT8
     elif m['cursorPos'] == 1:
-        cells.append(brlapi.DOT3 | brlapi.DOT6)
+        cursorDots = brlapi.DOT3 | brlapi.DOT6
     elif m['cursorPos'] == 2:
-        cells.append(brlapi.DOT2 | brlapi.DOT5)
+        cursorDots = brlapi.DOT2 | brlapi.DOT5
     elif m['cursorPos'] == 3:
-        cells.append(brlapi.DOT1 | brlapi.DOT4)
-    cells.append(0)
-    cells.append(0)
-    cells.append(0)
+        cursorDots = brlapi.DOT1 | brlapi.DOT4
 
-    # Display moving obstacles
-    # for cells in range(5, 30)
-    for _ in range(0, 5):
+    cursorObstacleColition = (m['cursorPos'] in [0, 1]) and (m['obstaclePos'] in [2, 3, 4])
+
+    # Calculate moving obstacles
+    # for cells in range(0, 30)
+    for i in range(0, 6):
+        cell00 = 0
+        cell01 = 0
+        cell02 = 0
+        cell03 = 0
+        cell04 = 0
+        
+        if i == 0:
+            cell01 = cursorDots
+        
         if m['obstaclePos'] == 0:
-            cells.append(brlapi.DOT3 | brlapi.DOT7)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT6 | brlapi.DOT8)
+            cell00 = brlapi.DOT3 | brlapi.DOT7
+            cell04 = brlapi.DOT6 | brlapi.DOT8
         elif m['obstaclePos'] == 1:
-            cells.append(brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
+            cell00 = brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8
         elif m['obstaclePos'] == 2:
-            cells.append(brlapi.DOT6 | brlapi.DOT8)
-            cells.append(brlapi.DOT3 | brlapi.DOT7)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
+            cell00 = brlapi.DOT6 | brlapi.DOT8
+            cell01 = cell01 | brlapi.DOT3 | brlapi.DOT7
         elif m['obstaclePos'] == 3:
-            cells.append(0)
-            cells.append(brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
+            cell01 = cell01 | brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8
         elif m['obstaclePos'] == 4:
-            cells.append(0)
-            cells.append(brlapi.DOT6 | brlapi.DOT8)
-            cells.append(brlapi.DOT3 | brlapi.DOT7)
-            cells.append(0)
-            cells.append(0)
+            cell01 = cell01 | brlapi.DOT6 | brlapi.DOT8
+            cell02 = brlapi.DOT3 | brlapi.DOT7
         elif m['obstaclePos'] == 5:
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8)
-            cells.append(0)
-            cells.append(0)
+            cell02 = brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8
         elif m['obstaclePos'] == 6:
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT6 | brlapi.DOT8)
-            cells.append(brlapi.DOT3 | brlapi.DOT7)
-            cells.append(0)
+            cell02 = brlapi.DOT6 | brlapi.DOT8
+            cell03 = brlapi.DOT3 | brlapi.DOT7
         elif m['obstaclePos'] == 7:
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8)
-            cells.append(0)
+            cell03 = brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8
         elif m['obstaclePos'] == 8:
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT6 | brlapi.DOT8)
-            cells.append(brlapi.DOT3 | brlapi.DOT7)
+            cell03 = brlapi.DOT6 | brlapi.DOT8
+            cell04 = brlapi.DOT3 | brlapi.DOT7
         elif m['obstaclePos'] == 9:
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(0)
-            cells.append(brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8)
+            cell04 = brlapi.DOT3 | brlapi.DOT7 | brlapi.DOT6 | brlapi.DOT8
+
+        if (i == 0) and cursorObstacleColition:
+            if (m['counter'] % 2 == 0):
+                cell00 = brlapi.DOT1 | brlapi.DOT3 | brlapi.DOT5 | brlapi.DOT8
+            else:            
+                cell00 = brlapi.DOT2 | brlapi.DOT7 | brlapi.DOT4 | brlapi.DOT6
+            cell01 = cell00
+            cell02 = cell00
+            cell03 = cell00
+            cell04 = cell00
+
+        # Display moving obstacles and player in position 1 
+        cells.append(cell00)
+        cells.append(cell01)
+        cells.append(cell02)
+        cells.append(cell03)
+        cells.append(cell04)
 
     # Display score
     # for cells in range(30, 40): 
@@ -160,8 +141,18 @@ def helper(m):
     cells.append(brlapi.DOT1 | brlapi.DOT2 | brlapi.DOT3 | brlapi.DOT6) # 0
     cells.append(brlapi.DOT1 | brlapi.DOT2 | brlapi.DOT3 | brlapi.DOT6) # 0
     cells.append(brlapi.DOT1 | brlapi.DOT2 | brlapi.DOT3 | brlapi.DOT6) # 0
+    
+    print(str(cells))
 
     return bytes(cells)
+
+# Visualize the model on the braille displan
+def view(brl, m):
+    printLog(m)
+    if m['counter'] == 0:
+        brl.writeText(m['message'])
+    else:
+        brl.writeDots(helper(m))
 
 def cursorUp(position):
     # Legal positions: 0..3, blocked
